3f49ae33df4011c4f0adb8805df9070adc4d5d2e
rener2104 <65041752+rener2104@users.noreply.github.com>
2020-11-17 13:11:15 -0600

    test

--- app/src/main/java/com/example/adhdapp3/homeScreen.kt
+++ app/src/main/java/com/example/adhdapp3/homeScreen.kt
@@ -6,6 +6,6 @@ import android.os.Bundle
 class homeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_screen )
+        setContentView(R.layout.activity_home_screen)
     }
 }
